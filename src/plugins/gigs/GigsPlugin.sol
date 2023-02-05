c95d96740617f97cb63385dabf7513ff61c0f545
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-02-05 16:59:12 +0400

    Fix inconsistent usage of license tag

--- src/plugins/gigs/GigsPlugin.sol
+++ src/plugins/gigs/GigsPlugin.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 import {LibDiamond} from "../../core/libraries/LibDiamond.sol";
--- src/plugins/gigs/queries/GigsGetCustomerQuery.sol
+++ src/plugins/gigs/queries/GigsGetCustomerQuery.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 import {AppStorage, PersonEntity, LibAppStorage} from "../../../core/libraries/LibAppStorage.sol";
